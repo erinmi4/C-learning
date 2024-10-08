a4a556779e373936995b5e7488a68dbe564a4fd9
mika <mikasa2511592510@gmail.com>
2024-10-08 21:54:10 +0800

    完成input和数字打印

--- CS106B/Lecture1/main.cpp
+++ CS106B/Lecture1/main.cpp
@@ -2,10 +2,20 @@
 #include "console.h"
 using namespace std;
 
+
+int testInput() {
+    int limit;
+    cout << "please input what you want:";
+    cin >> limit;
+    return limit;
+}
 // The C++ compiler will look for a function
 // called "main"
 int main() {
-   cout << "Hello, world!" << endl;
-   return 0;  // must return an int to indicate
+    int limit = 0;
+    limit = testInput();
+    int result = limit * limit;
+    cout << "The two of power is " <<result;
+    return 0;  // must return an int to indicate
               // successful program completion
 }
